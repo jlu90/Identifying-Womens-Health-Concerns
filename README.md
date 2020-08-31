05d313e983b1984c33ce331a144aac9e06a97040
Jocelyn Lutes <jocelynlutes@Jocelyns-MacBook-Pro-2.local>
2020-08-31 17:44:53 -0400

    added read me and modifed clustering algorithms

---
+++ README.md
@@ -0,0 +1,15 @@
+![woman](assets/woman_1.png) 
+
+# Topic Modeling to Identify Women's Health Concerns on Online Forums
+
+## Background
+
+## Problem Statement
+
+## Executive Summary
+
+## Contents of Repository
+
+## Data Dictionary
+
+## Conclusions and Recommendations
---
+++ code/capstone_functions.py
@@ -0,0 +1,14 @@
+# This file contains the code for functions used in "Topic Modeling to Identify Women's Health Concerns"
+# Functions are organized by section of the project
+
+### DATA CLEANING ###
+
+### TEXT PREPROCESSING ###
+
+### EXPLORATORY DATA ANALYSIS ###
+
+### MODEL PREPARATION ###
+
+### MODELING ###
+
+### MODEL SELECTION AND EVALUATION ###
